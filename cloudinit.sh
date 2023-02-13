9536e838c8b2d48eef994bec12e46c9dfb6965d0
Kevin <kevinvillacres97@gmail.com>
2023-02-13 01:42:52 +0100

    Initial commit

---
+++ cloudinit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+amazon-linux-extras install -y docker
+service docker start
+systemctl enable docker
+usermod -a -G docker ec2-user
+pip3 install docker-compose
+mkdir /home/ec2-user/hello-2048
+cd /home/ec2-user/hello-2048
+wget https://raw.githubusercontent.com/qebyn/hello-2048/main/docker-compose.yml
+docker-compose pull
+docker-compose up -d
